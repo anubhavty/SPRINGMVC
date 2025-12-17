b4b2689da7c7d30149444e308603c9f8cd9a97db
Anubhav Tyagi <tyagiianii009@gmail.com>
2025-12-17 17:43:44 +0530

    Initial commit

---
+++ src/test/java/com/anubhavtyagi28/SPRINGMVC/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anubhavtyagi28.SPRINGMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
