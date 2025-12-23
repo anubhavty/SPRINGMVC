c20542a72df6ba9b3e5b601e8daf3f7eb510c7ae
Anubhav Tyagi <tyagiianii009@gmail.com>
2025-12-23 23:15:09 +0530

    Service Layer

--- src/main/java/com/anubhavtyagi28/SPRINGMVC/controllers/EmployeeController.java
+++ src/main/java/com/anubhavtyagi28/SPRINGMVC/controllers/EmployeeController.java
@@ -1,11 +1,9 @@
 package com.anubhavtyagi28.SPRINGMVC.controllers;
 
 import com.anubhavtyagi28.SPRINGMVC.dto.EmployeeDTO;
-import com.anubhavtyagi28.SPRINGMVC.entities.EmployeeEntity;
-import com.anubhavtyagi28.SPRINGMVC.repositories.EmployeeRepository;
+import com.anubhavtyagi28.SPRINGMVC.services.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -15,21 +13,21 @@ public class EmployeeController {
 //    public String getMySuperSecretMessage() {
 //        return "Secret Message: a55sgu6ygd78$^%%&#$";
 //    }
-    private final EmployeeRepository employeeRepository;
-    public EmployeeController(EmployeeRepository employeeRepository){
-        this.employeeRepository = employeeRepository;
+    private final EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
     }
     @GetMapping(path = "/{employeeId}")
-    public EmployeeEntity getEmployeeById(@PathVariable(name="employeeId") Long id){
-        return employeeRepository.findById(id).orElse(null);
+    public EmployeeDTO getEmployeeById(@PathVariable(name="employeeId") Long id){
+        return employeeService.getEmployeeById(id);
     }
     @GetMapping
-    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
-        return employeeRepository.findAll();
+    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
+        return employeeService.getAllEmployees();
     }
     @PostMapping
-    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee){
-        return employeeRepository.save(inputEmployee);
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+        return employeeService.createNewEmployee(inputEmployee);
     }
     @PutMapping
     public String updateEmployeeById(){
--- src/main/java/com/anubhavtyagi28/SPRINGMVC/dto/EmployeeDTO.java
+++ src/main/java/com/anubhavtyagi28/SPRINGMVC/dto/EmployeeDTO.java
@@ -8,20 +8,20 @@ public class EmployeeDTO {
     private String email;
     private Integer age;
     private LocalDate dateOfJoining;
-    private Boolean isActive;
+    private Boolean active;
 
     public EmployeeDTO() {
 
     }
 
 
-    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean isActive) {
+    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean active) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.age = age;
         this.dateOfJoining = dateOfJoining;
-        this.isActive = isActive;
+        this.active = active;
     }
 
     public Long getId() {
@@ -65,10 +65,10 @@ public class EmployeeDTO {
     }
 
     public Boolean getActive() {
-        return isActive;
+        return active;
     }
 
     public void setActive(Boolean active) {
-        isActive = active;
+        this.active = active;
     }
 }
